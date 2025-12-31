3fddb956bedcf8c9490c2163341e0a3901e83626
marieiram <marieiram123@icloud.com>
2025-12-31 14:27:45 +0900

    Devise 認をるいk証

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_31_032535) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_31_045602) do
   create_table "comments", force: :cascade do |t|
     t.text "body", null: false
     t.datetime "created_at", null: false
@@ -24,6 +24,18 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_31_032535) do
     t.datetime "created_at", null: false
     t.string "title", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"

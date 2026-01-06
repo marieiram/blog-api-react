50e92df1e639024e7969df2cafc5edfbe80e3c17
marieiram <marieiram123@icloud.com>
2026-01-06 19:39:25 +0900

    feat: add posts and comments APIs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,18 @@ class Comment < ApplicationRecord
   belongs_to :user
   validates :body, presence: true
   validates :user, presence: true
+
+
+    # API用JSON形式でのデータ返却
+    def api_json
+    {
+      id: id,
+      body: body,
+      created_at: created_at,
+      user: {
+        id: user.id,
+        email: user.email
+    }
+     }
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,18 @@ class Post < ApplicationRecord
     validates :body, presence: true
     has_many :comments, dependent: :destroy
     belongs_to :user
+
+    # API用JSON形式でのデータ返却
+    def api_json
+    {
+      id: id,
+      title: title,
+      body: body,
+      created_at: created_at,
+      user: {
+        id: user.id,
+        email: user.email
+      }
+    }
+  end
 end

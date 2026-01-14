03e5ce6563ff2fc18a6dace4955b7e7ef3911254
marieiram <marieiram123@icloud.com>
2026-01-14 09:24:57 +0900

    Revert "一覧取得とログアウトを追加"
    This reverts commit 36112bcdfd7666b0f4183bf91703cdbbcfcc7dff.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,13 @@
-//値を保存して、画面と連動させる仕組み
-import { useState ,useEffect} from "react";
+import { useState } from "react";
 
 function App() {
-//状態の定義
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
-  //tokenがあるかどうか（ログイン状態を知るため）
-  const [token,setToken] = useState(localStorage.getItem("token"));
-  //投稿一覧用
-  const [posts, setPosts] = useState([])
 
-  //ログイン処理
   const handleLogin = async (e) => {
-    //通常<form>は送信すると、ページがリロードされるが、Reactではそれを止める
     e.preventDefault();
 
-    //railsのログインAPI
     const res = await fetch("http://localhost:3000/api/sessions", {
       method: "POST",
       headers: {
@@ -28,58 +19,21 @@ function App() {
       }),
     });
 
-    //レスポンス処理 Railsが返したjsonをオブジェクトに返す
     const data = await res.json();
-    //成功時　
+
     if (res.ok) {
-      //ローカルストレージに保存（ブラウザに永久保存）
       localStorage.setItem("token", data.token);
-      setToken(data.token);
-      //画面更新
       setMessage("ログイン成功！");
-    } 
-    //失敗時
-    else {
+    } else {
       setMessage("ログイン失敗");
     }
   };
 
-
-  //投稿取得関数
-  const fetchPosts = async () => {
-    const token = localStorage.getItem("token");
-    
-    const res = await fetch("http://localhost:3000/api/posts",{
-      headers:{
-        "Authorization": `Bearer ${token}`,
-      }
-    })
-  const data = await res.json();
-  setPosts(data);
-}
-
-  //tokenが変化したら実行 ログイン後に投稿取得
-  useEffect(()=> {
-    if (token){
-      fetchPosts();
-    }
-  },[token]);
-
-   // ログアウト
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setToken(null);
-    setPosts([]);
-  };
-
-  
   return (
     <div style={{ padding: "40px" }}>
+      <h1>ログイン</h1>
 
-{!token ? (
-   //ログイン前の画面
-   <form onSubmit={handleLogin}>
-         <h1>ログイン</h1>
+      <form onSubmit={handleLogin}>
         <div>
           <input
             type="email"
@@ -100,36 +54,10 @@ function App() {
 
         <button type="submit">ログイン</button>
       </form>
-     
-      
-    ) : (
-    
-      //ログイン後の画面
-     <div>
-        <h1>ログイン済み</h1>
-        <p>ようこそ！</p>
-        <button onClick={handleLogout}>ログアウト</button>
-        <h2>投稿</h2>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>
-              <h3>{post.title}</h3>
-              <p>{post.body}</p>
-              <small>作成者: {post.user.email}</small>
-            </li>
-          ))}
-        </ul>
-     </div>
 
-  )}  
-      
-     
- {/* message が空 → 表示しない・message がある → 表示 */}
       {message && <p>{message}</p>}
     </div>
-
   );
 }
 
-//他ファイル（main.jsx）から <App /> として使える
 export default App;
